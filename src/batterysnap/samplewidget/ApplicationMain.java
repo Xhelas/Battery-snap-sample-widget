649dc6fc2f8d4c1220af2c37cff8522b4ad6bcf6
alenoir <alexandre.lenoir@parrot.com>
2015-03-02 17:56:04 +0100

    manage service start

--- src/batterysnap/samplewidget/ApplicationMain.java
+++ src/batterysnap/samplewidget/ApplicationMain.java
@@ -7,22 +7,43 @@ import android.content.Intent;
 import android.util.Log;
 
 public class ApplicationMain extends Application {
-  private static final String tag = ApplicationMain.class.getSimpleName();
+    private static final String tag = ApplicationMain.class.getSimpleName();
+    private static ApplicationMain _instance;
+    private boolean _keepAlive;
 
-  @Override
-  public void onCreate() {
-    int widgetCount = getWidgetCount();
-    if (BuildConfig.DEBUG) {
-      Log.d(tag, "Found " + widgetCount + " widgets.");
+    @Override
+    public void onCreate() {
+        _instance = this;
+        int widgetCount = getWidgetCount();
+        if (BuildConfig.DEBUG) {
+            Log.d(tag, "Found " + widgetCount + " widgets.");
+        }
+        if (widgetCount > 0) {
+        }
     }
-    if (widgetCount > 0) {
-      startService(new Intent(this, WidgetService.class));
-    }
-  }
 
-  private int getWidgetCount() {
-    AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
-    return appWidgetManager.getAppWidgetIds(new ComponentName(this, BatterySnapWidgetProvider.class)).length;
-  }
+    private int getWidgetCount() {
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        return appWidgetManager.getAppWidgetIds(new ComponentName(this, BatterySnapWidgetProvider.class)).length;
+    }
+
+    static ApplicationMain getInstance(){
+        return _instance;
+    }
+
+    void startService(){
+        startService(new Intent(this, WidgetService.class));
+        _keepAlive = true;
+        if (BuildConfig.DEBUG) {
+            Log.d(tag, "Service is started");
+        }
+    }
+    void stopService(){
+        stopService(new Intent(this, WidgetService.class));
+        _keepAlive =false;
+        if (BuildConfig.DEBUG) {
+            Log.d(tag, "Service is stopped");
+        }
+    }
 
 }
